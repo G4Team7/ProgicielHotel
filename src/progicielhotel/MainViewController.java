97a87e9292cde8220f14015f57935092e957ff04
Rechad Sanehy <rechad.sanehy@gmail.com>
2016-06-21 18:00:49 +0200

    Ajout Login

---
+++ src/progicielhotel/MainViewController.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package progicielhotel;
+
+import javafx.event.*;
+import javafx.fxml.FXML;
+import javafx.scene.control.*;
+
+/**
+ * FXML Controller class
+ *
+ * @author Rechad
+ */
+public class MainViewController{
+
+    @FXML private Button logoutButton;
+  @FXML private Label  sessionLabel;
+  
+  public void initialize() {}
+  
+  public void initSessionID(final LoginManager loginManager, String sessionID) {
+    sessionLabel.setText(sessionID);
+    logoutButton.setOnAction(new EventHandler<ActionEvent>() {
+      @Override public void handle(ActionEvent event) {
+        loginManager.logout();
+      }
+    });
+  }
+    
+}
